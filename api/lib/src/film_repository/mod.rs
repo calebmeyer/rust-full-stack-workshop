b302e1cb6682b97f44816c89fac599baf4ab2c9a
Caleb Meyer <Kiaulen@gmail.com>
2023-07-08 21:58:28 -0500

    Add film repository trait

---
+++ api/lib/src/film_repository/mod.rs
@@ -0,0 +1,14 @@
+use shared::models::{CreateFilm, Film};
+use uuid::Uuid;
+
+pub type FilmError = String;
+pub type FilmResult<T> = Result<T, FilmError>;
+
+#[async_trait::async_trait]
+pub trait FilmRepository: Send + Sync + 'static {
+    async fn get_all(&self) -> FilmResult<Vec<Film>>;
+    async fn get_by_id(&self, id: &Uuid) -> FilmResult<Film>;
+    async fn create(&self, body: &CreateFilm) -> FilmResult<Film>;
+    async fn update(&self, body: &Film) -> FilmResult<Film>;
+    async fn destroy(&self, id: &Uuid) -> FilmResult<Uuid>;
+}
