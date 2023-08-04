15988a80ae7ad4cf3f79d2a000aa1af7e73d3880
Caleb Meyer <caleb.meyer@hy-vee.com>
2023-08-03 20:39:35 -0500

    implement film endpoints

--- api/lib/src/films.rs
+++ api/lib/src/films.rs
@@ -2,6 +2,8 @@ use actix_web::{
     web::{self, ServiceConfig},
     HttpResponse,
 };
+use shared::models::{CreateFilm, Film};
+use uuid::Uuid;
 
 pub fn service(cfg: &mut ServiceConfig) {
     cfg.service(
@@ -14,22 +16,55 @@ pub fn service(cfg: &mut ServiceConfig) {
     );
 }
 
-async fn index() -> HttpResponse {
-    HttpResponse::Ok().finish()
+async fn index(
+    repo: actix_web::web::Data<Box<dyn crate::film_repository::FilmRepository>>,
+) -> HttpResponse {
+    match repo.get_all().await {
+        Ok(films) => HttpResponse::Ok().json(films),
+        Err(e) => HttpResponse::NotFound().body(format!("Internal server error: {:?}", e)),
+    }
 }
 
-async fn show() -> HttpResponse {
-    HttpResponse::Ok().finish()
+async fn show(
+    repo: actix_web::web::Data<Box<dyn crate::film_repository::FilmRepository>>,
+    id: web::Path<Uuid>,
+) -> HttpResponse {
+    match repo.get_by_id(&id).await {
+        Ok(film) => HttpResponse::Ok().json(film),
+        Err(e) => HttpResponse::NotFound().body(format!("Internal server error: {:?}", e)),
+    }
 }
 
-async fn create() -> HttpResponse {
-    HttpResponse::Ok().finish()
+async fn create(
+    repo: actix_web::web::Data<Box<dyn crate::film_repository::FilmRepository>>,
+    film_body: web::Json<CreateFilm>,
+) -> HttpResponse {
+    match repo.create(&film_body).await {
+        Ok(film) => HttpResponse::Ok().json(film),
+        Err(e) => {
+            HttpResponse::InternalServerError().body(format!("Internal server error: {:?}", e))
+        }
+    }
 }
 
-async fn update() -> HttpResponse {
-    HttpResponse::Ok().finish()
+async fn update(
+    repo: actix_web::web::Data<Box<dyn crate::film_repository::FilmRepository>>,
+    film_body: web::Json<Film>,
+) -> HttpResponse {
+    match repo.update(&film_body).await {
+        Ok(film) => HttpResponse::Ok().json(film),
+        Err(e) => {
+            HttpResponse::InternalServerError().body(format!("Internal server error: {:?}", e))
+        }
+    }
 }
 
-async fn destroy() -> HttpResponse {
-    HttpResponse::Ok().finish()
+async fn destroy(
+    repo: actix_web::web::Data<Box<dyn crate::film_repository::FilmRepository>>,
+    id: web::Path<Uuid>,
+) -> HttpResponse {
+    match repo.destroy(&id).await {
+        Ok(id) => HttpResponse::Ok().json(id),
+        Err(e) => HttpResponse::NotFound().body(format!("Internal server error: {:?}", e)),
+    }
 }
